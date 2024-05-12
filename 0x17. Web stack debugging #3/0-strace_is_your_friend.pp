c94bc2b5fd53c0b631b4d6ce34978bb44dc769c5
Tshoane <140214564+Tshoane@users.noreply.github.com>
2024-05-12 20:55:58 +0200

    Update 2 0-strace_is_your_friend.pp

--- 0x17. Web stack debugging #3/0-strace_is_your_friend.pp
+++ 0x17. Web stack debugging #3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# automated puppet fix (to find out why Apache is returning a 500 error)
+# Debug apache2
 
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
+exec { 'fix_wp_locale':
+  command => "/bin/sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
+  path    => ['/bin', '/usr/bin'],
 }
