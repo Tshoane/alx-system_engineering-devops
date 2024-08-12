63d8ea329804fd407d6fd880b9c8a23f3cdf5046
Tshoane <140214564+Tshoane@users.noreply.github.com>
2024-08-12 03:08:33 +0200

    Delete 0x17. Web stack debugging #3 directory

--- 0x17. Web stack debugging #3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Debug apache2
-
-exec { 'fix_wp_locale':
-  command => "/bin/sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
-  path    => ['/bin', '/usr/bin'],
-}
